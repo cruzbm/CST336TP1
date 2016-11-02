70562b4e55e7b72be407cc1004912a331602b3ca
Brandon Cruz <brcruz@csumb.edu>
2016-11-02 19:58:15 +0000

    Now prints just the two specific brands available. Will double print if there are more than two brands...

--- indexP1.php
+++ indexP1.php
@@ -5,28 +5,27 @@
     $dbConn = getDatabaseConnection('tp336');
     
     function getBrands(){
+        global $dbConn;
+        
         //Print list of all brands
-        $sql = "SELECT name
-                FROM shoes";
+        $sql = "SELECT name, brand
+                FROM Shoes";
                 
         $stmt = $dbConn->prepare($sql);
-        $stmt = execute();
-        $records = fetchAll(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
         echo "Brands Available: <br />";
         $brands = array();
-        foreach ($records as $brand){
-            for ($i = 0; $i < count($brands); $i++)
-            {
-                if($brands[$i] == $brand['name']){
-                    break;
-                }
-                else{
-                    $brands[] = $brand['name'];
-                    echo $brand['name'] . "<br />";
-                }
+        foreach($records as $brand){
+            $brands[] = $brand['brand'];
+        }
+        
+        for ($i = 1; $i < count($brands); $i++){
+            if ($brands[$i] != $brands[$i-1]){
+                echo $brands[$i-1] . "<br />";
+                echo $brands[$i] . "<br />";
             }
-            
         }
     }
     
