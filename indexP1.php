434acc47944a900ada04681f64f47c792cc15904
Brandon Cruz <brcruz@csumb.edu>
2016-11-02 00:03:11 +0000

    Index file for project added, no real code yet.

---
+++ indexP1.php
@@ -0,0 +1,16 @@
+<?php
+    include "../../Includes/dbConnection.php";
+    
+    
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Project 1 Main</title>
+    </head>
+    <body>
+        <h1>ShoeShopper Online</h1>
+    </body>
+</html>
