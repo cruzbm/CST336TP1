647def089c39a9909fd5092a5095a2adf2c6ac47
Brandon Cruz <brcruz@csumb.edu>
2016-11-02 19:20:30 +0000

    Some code running but not functioning properly. I'm trying to figure out why the connection won't prepare properly.

--- indexP1.php
+++ indexP1.php
@@ -1,5 +1,34 @@
 <?php
+
+    //Database Connection established
     include "../../Includes/dbConnection.php";
+    $dbConn = getDatabaseConnection('tp336');
+    
+    function getBrands(){
+        //Print list of all brands
+        $sql = "SELECT name
+                FROM shoes";
+                
+        $stmt = $dbConn->prepare($sql);
+        $stmt = execute();
+        $records = fetchAll(PDO::FETCH_ASSOC);
+        
+        echo "Brands Available: <br />";
+        $brands = array();
+        foreach ($records as $brand){
+            for ($i = 0; $i < count($brands); $i++)
+            {
+                if($brands[$i] == $brand['name']){
+                    break;
+                }
+                else{
+                    $brands[] = $brand['name'];
+                    echo $brand['name'] . "<br />";
+                }
+            }
+            
+        }
+    }
     
     
 ?>
@@ -7,10 +36,16 @@
 
 <!DOCTYPE html>
 <html>
+    //Include css here
+    
     <head>
-        <title>Project 1 Main</title>
+        <title>ShoeShopper Main Page</title>
     </head>
     <body>
         <h1>ShoeShopper Online</h1>
+        
+        <!-- initial print of available brands -->
+        <?=getBrands()?>
+        
     </body>
 </html>
