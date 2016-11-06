af2b5186adbf924f4944a838a6df6b53523e3782
Brandon Cruz <brcruz@csumb.edu>
2016-11-06 20:34:20 +0000

    Changed @import line to <link>tag

--- indexP1.php
+++ indexP1.php
@@ -4,6 +4,10 @@
     include "../../Includes/dbConnection.php";
     $dbConn = getDatabaseConnection('tp336');
     
+    function displayImgs(){
+        
+    }
+    
     function getBrands(){
         global $dbConn;
         
@@ -15,18 +19,19 @@
         $stmt->execute();
         $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
-        echo "Brands Available: <br />";
         $brands = array();
         foreach($records as $brand){
             $brands[] = $brand['brand'];
         }
         
+        echo "<hr><h3><font color='white' face='arial black'>";
         for ($i = 1; $i < count($brands); $i++){
             if ($brands[$i] != $brands[$i-1]){
-                echo $brands[$i-1] . "<br />";
+                echo $brands[$i-1] . " | ";
                 echo $brands[$i] . "<br />";
             }
         }
+        echo "</font></h3><hr>";
     }
     
     
@@ -35,18 +40,23 @@
 
 <!DOCTYPE html>
 <html>
-    //Include css here
+    <!-- Include css here -->
     
     <head>
         <title>ShoeShopper Main Page</title>
     </head>
+    
+    <link rel="stylesheet" href="p1.css"> </link>
+    
     <body>
         <h1>ShoeShopper Online</h1>
         
         <!-- initial print of available brands -->
         <?=getBrands()?>
           <?=displayImgs()?>
-        
+        <form>
+            
+        <!---------------------Menu Bar------------------------------------->    
         <fieldset>
             <legend>Filter Options</legend>
             Price     
@@ -68,10 +78,12 @@
                  <option value="flats">Flats</option>
             </select>
             
-            
-            
+            <br />
+            <input type="submit" name="Submit" value="Apply" />
         </fieldset>
+        <!------------------------------------------------------------------>
         
+        </form>
         
     </body>
 </html>
