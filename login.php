7cfa80ac01197be47a04338aa1c3d71cced36bc3
Austin Thao <athao@csumb.edu>
2016-11-08 18:02:18 +0000

    login

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Online Phone Catalog</title>
+    </head>
+    <body>
+        <h1> Smart Phones for Sale!</h1>
+         <link rel="stylesheet" href="tp1.css" type="text/css" />
+        <form action="process.php"  method="post">
+            Username: <input type="text" name="username" /> 
+            <br />
+            Password: <input type="password" name="password"  />
+            <br />
+            <input type="submit" name="loginForm" value="Login"/>
+        </form>
+
+    </body>
+</html>
