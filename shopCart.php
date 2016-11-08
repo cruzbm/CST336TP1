2427c7cbc4b0ac070047cf6eb2a4c16163309fdc
Austin Thao <athao@csumb.edu>
2016-11-08 18:03:12 +0000

    shopCart

---
+++ shopCart.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Online Phone Catalog</title>
+    </head>
+    <body>
+         <link rel="stylesheet" href="tp1.css" type="text/css" />
+        <h1> Shopping Cart</h1>
+            
+            
+            
+        </form>
+
+    </body>
+</html>
